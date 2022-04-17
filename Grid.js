b212ee371584fee61fdfbb9feb9a535b10ca9eb4
Sodu <m.sodoo9911@gmail.com>
2022-04-17 16:13:21 +0800

    Final

--- Grid.js
+++ Grid.js
@@ -9,14 +9,42 @@ export default class Grid {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
-    this.#cells = createElement(gridElement).map((cellElement, index) => {
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
       return new Cell(
         cellElement,
         index % GRID_SIZE,
         Math.floor(index / GRID_SIZE)
       );
     });
-    console.log(this.cells);
+  }
+
+  get cells() {
+    return this.#cells;
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter((cell) => cell.tile == null);
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+    return this.#emptyCells[randomIndex];
   }
 }
 
@@ -24,15 +52,61 @@ class Cell {
   #cellElement;
   #x;
   #y;
+  #tile;
+  #mergeTile;
 
-  constructor(cell, x, y) {
-    this.#cell = cell;
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement;
     this.#x = x;
     this.#y = y;
   }
+
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
+  }
+
+  get tile() {
+    return this.#tile;
+  }
+
+  set tile(value) {
+    this.#tile = value;
+    if (value == null) return;
+    this.#tile.x = this.#x;
+    this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    );
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return;
+    this.tile.value = this.tile.value + this.mergeTile.value;
+    this.mergeTile.remove();
+    this.mergeTile = null;
+  }
 }
 
-function createElement(gridElement) {
+function createCellElements(gridElement) {
   const cells = [];
   for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
     const cell = document.createElement("div");
